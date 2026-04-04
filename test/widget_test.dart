bfca7e6995a825c083a9e095155e1e8201212f15
MrPixel555 <kasra.rahimbeiky@gmail.com>
2026-04-04 09:24:22 +0330

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,4 @@
-import 'package:aw_manager_ui/main.dart';
+import 'package:alpha_wet/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,9 +10,9 @@ void main() {
     SharedPreferences.setMockInitialValues(<String, Object>{});
 
     await tester.pumpWidget(const AwManagerApp());
-    await tester.pumpAndSettle();
+    await tester.pump();
 
-    expect(find.text('AlphaWet'), findsOneWidget);
+    expect(find.text('AlphaWet'), findsWidgets);
     expect(find.textContaining('HTTP 127.0.0.1:10808'), findsOneWidget);
     expect(find.text('made by AlphaCraft'), findsOneWidget);
     expect(find.byIcon(Icons.tune_rounded), findsOneWidget);

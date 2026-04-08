7d19b59267dfc7da52fc2371cb292f4a626c1b52
MrPixel555 <kasra.rahimbeiky@gmail.com>
2026-04-08 23:45:01 +0330

    update

--- lib/runtime/desktop_xray_runtime.dart
+++ lib/runtime/desktop_xray_runtime.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 
